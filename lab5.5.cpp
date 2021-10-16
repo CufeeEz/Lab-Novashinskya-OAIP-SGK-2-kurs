71447af0fc0f7bf5fb358ee508262fa2b8256867
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-10-16 17:43:13 +0300

    Add files via upload

---
+++ lab5.5.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a[50], c = 0, b = 0, ss, one = 26, two = -8;
+    srand(time(NULL));
+    cout << "Введите число: ";
+    cin >> ss;
+    if (ss < 0 or ss > 50)
+    {
+        cout << "Некорректный запрос.";
+    }
+    else
+    {
+        for (int i = 0; i < ss; i++)
+        {
+            a[i] = rand() % (one - two) + two;
+            cout << a[i] << " ";
+        }
+    }
+    return 0;
+}

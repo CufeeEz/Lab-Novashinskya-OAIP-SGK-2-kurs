d9bd643ce7c65833ccbd9f9c1ef5bb5c5aead103
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-10-16 17:11:28 +0300

    Add files via upload

---
+++ lab5.4.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a[50], c = 0, b;
+    for (int i = 0; i < 50; i++)
+    {
+        a[i] = rand() % 11 - 5;
+        cout << a[i] << " ";
+        if (a[i] < 0)
+        {
+            c += 1;
+        }
+    }
+    cout << "\n" << "Количество отрицательных чисел = " << c;
+    return 0;
+}

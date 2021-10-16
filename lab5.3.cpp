a0fe891bef0c18af13fc1935e54880d84a7a6f9c
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-10-16 16:40:40 +0300

    Add files via upload

---
+++ lab5.3.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a[100], c = 0, b;
+    cout << "Введите число: ";
+    cin >> b;
+    b += 1;
+    if (b > 0 and b < 100)
+    {
+        for (int i = 0; i < b; i++)
+        {
+            a[i] = i;
+            if (a[i] % 2 == 0)
+            {
+                cout << a[i] << "*" << 2 << "=" << a[i] * 2 << endl;
+            }
+        }
+        for (int i = 0; i < b; i++)
+        {
+            if (a[i] % 2 == 1)
+            {
+                cout << a[i] << "*" << 3 << "=" << a[i] * 3 << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "Некорректный запрос.";
+    }
+    return 0;
+}

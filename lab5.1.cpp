e88d412c5c2f571521e23bb864360ead13c1473b
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-11-10 15:12:04 +0300

    Update lab5.1.cpp

--- lab5.1.cpp
+++ lab5.1.cpp
@@ -3,25 +3,23 @@ using namespace std;
 int main()
 {
     setlocale(0, "Russian");
-    int a[100], c = 0, b;
-    cout << "Введите число: ";
-    cin >> b;
-    b += 1;
-    if (b > 0 and b < 100)
+    int a[5], c = 0, b;
+    cout << "Введите пять целых чисел в массив: ";
+    for (int i = 0; i < 5; i++)
     {
-        for (int i = 0; i < b; i++)
+        cin >> a[i];
+        if (a[i] % 5 == 0)
         {
-            a[i] = i;
-            if (a[i] % 5 == 0)
-            {
-                c = a[i] + c;
-            }
+            c = a[i] + c;
         }
+    }
+    if (c == 0)
+    {
+        cout << "В массиве не найдено чисел кратных 5";
     }
     else
     {
-        cout << "Некорректный запрос.";
+        cout << "Сумма чисел кратных 5 = " << c << endl;
     }
-    cout << c;
     return 0;
 }

d56793ca275b5a4180a1734135bea2a32f99ca77
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-12-01 13:20:12 +0300

    Add files via upload

---
+++ lab5.9.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(0, "Russian");
+    int a[5][5], summ = 0, maax[5], max = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        for (int ii = 0; ii < 5; ii++)
+        {
+            a[i][ii] = rand() % 40;
+            cout << a[i][ii] << " ";
+            if (ii == 4)
+            {
+                cout << endl;
+            }
+        }
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        for (int ii = 0; ii < 5; ii++)
+        {
+            if (i + ii == 4)
+            {
+                if (max < a[i][ii])
+                {
+                    max = a[i][ii];
+                }
+            }
+        }
+    }
+    cout << "Максимальный элементов двумерного массива находящихся в дополнительной диагонали = " << max << endl;
+    return 0;
+}

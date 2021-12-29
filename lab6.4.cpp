33d36d4a005b7d124e783974ebf8951e29de6576
Daniil <92633278+CufeeEz@users.noreply.github.com>
2021-12-29 14:19:55 +0300

    Add files via upload

---
+++ lab6.4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int notx(int n, float x)
+{
+    float resul = pow(x,n) / n;
+    return resul;
+}
+int main()
+{
+    setlocale(0, "Russian");
+    int x;
+    int n;
+    cout << "Введите число x" << endl;
+    cin >> x;
+    cout << "Введите число n" << endl;
+    cin >> n;
+    float f = notx(n, x);
+    cout << "Результат функции = " << f << endl;
+    int rez = pow(f, 2) / 2 + pow(f, 4) / 4 + pow(f, 6) / 6;
+    cout << "Ответ = " << rez;
+}
